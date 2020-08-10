0fcab2a72379fecdfb765ca8a97cafa3b02e76dd
dwiechert <dan.wiechert@gmail.com>
2020-08-09 21:13:34 -0500

    Separated classfile to separate struct.

---
+++ src/classfile.rs
@@ -0,0 +1,37 @@
+pub struct ClassFile {
+    magic_number: u32,
+    minor_version: u16,
+    major_version: u16,
+    constant_pool_count: u16,
+}
+
+impl ClassFile {
+    pub fn new(
+        magic_number: u32,
+        minor_version: u16,
+        major_version: u16,
+        constant_pool_count: u16) -> Self {
+        Self {
+            magic_number,
+            minor_version,
+            major_version,
+            constant_pool_count,
+        }
+    }
+
+    pub fn magic_number(&self) -> &u32 {
+        &self.magic_number
+    }
+
+    pub fn minor_version(&self) -> &u16 {
+        &self.minor_version
+    }
+
+    pub fn major_version(&self) -> &u16 {
+        &self.major_version
+    }
+
+    pub fn constant_pool_count(&self) -> &u16 {
+        &self.constant_pool_count
+    }
+}
--- src/reader.rs
+++ src/reader.rs
@@ -1,5 +1,7 @@
 use std::fs::{File, OpenOptions};
 use std::io::{Read};
+use crate::classfile;
+use crate::classfile::ClassFile;
 
 pub struct Reader {
     file: File,
@@ -30,6 +32,28 @@ impl Reader {
             }
         }
     }
+
+    pub fn read_class_file(&mut self) -> ClassFile {
+        let bytes = self.read_file();
+
+        let mut magic_array: [u8; 4] = Default::default();
+        magic_array.copy_from_slice(&bytes[0..4]);
+        let magic_int = u32::from_be_bytes(magic_array);
+
+        let mut minor_array: [u8; 2] = Default::default();
+        minor_array.copy_from_slice(&bytes[4..6]);
+        let minor_int = u16::from_be_bytes(minor_array);
+
+        let mut major_array: [u8; 2] = Default::default();
+        major_array.copy_from_slice(&bytes[6..8]);
+        let major_int = u16::from_be_bytes(major_array);
+
+        let mut cpc_array: [u8; 2] = Default::default();
+        cpc_array.copy_from_slice(&bytes[8..10]);
+        let cpc_int = u16::from_be_bytes(cpc_array);
+
+        ClassFile::new(magic_int, minor_int, major_int, cpc_int)
+    }
 }
 
 #[cfg(test)]
@@ -49,25 +73,22 @@ mod tests {
 
         let filename = path.as_os_str().to_str().unwrap().to_string();
         let mut reader = Reader::new(&filename);
-        let bytes = reader.read_file();
-        println!("Bytes: {:?}", bytes);
+        let mut class_file = reader.read_class_file();
 
-        let mut magic_array: [u8; 4] = Default::default(); //bytes.split_at(4).0;
-        magic_array.copy_from_slice(&bytes[0..4]);
-        let magic_int = u32::from_be_bytes(magic_array);
+        let magic_int = class_file.magic_number();
         println!("Magic: {}", magic_int);
-        assert_eq!(0xCAFEBABE, magic_int);
+        assert_eq!(&0xCAFEBABE, magic_int);
 
-        let mut minor_array: [u8; 2] = Default::default();
-        minor_array.copy_from_slice(&bytes[4..6]);
-        let minor_int = i16::from_be_bytes(minor_array);
+        let minor_int = class_file.minor_version();
         println!("Minor: {}", minor_int);
-        assert_eq!(0, minor_int);
+        assert_eq!(&0, minor_int);
 
-        let mut major_array: [u8; 2] = Default::default();
-        major_array.copy_from_slice(&bytes[6..8]);
-        let major_int = i16::from_be_bytes(major_array);
+        let major_int = class_file.major_version();
         println!("Major: {}", major_int);
-        assert_eq!(58, major_int);
+        assert_eq!(&58, major_int);
+
+        let cpc_int = class_file.constant_pool_count();
+        println!("Constant Pool Count: {}", cpc_int);
+        assert_eq!(&29, cpc_int);
     }
 }
